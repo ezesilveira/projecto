b6fcb76c8681e78ceb305a1f859fe02209892440
Edgardo Ezequiel Silveira <103297004+ezesilveira@users.noreply.github.com>
2023-09-20 23:58:52 -0300

    Componentes + Silveira

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent,
+    ToolBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
