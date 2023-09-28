c0d488447495aa63a2d20369f59e518930d164c6
Edgardo Ezequiel Silveira <103297004+ezesilveira@users.noreply.github.com>
2023-09-28 03:10:56 -0300

    Lista + Silveira

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
 
+  usuarios = [
+    {id : 2342, nombre : 'Jac', apellido: 'Goodman', estado : 'Activo'},
+    {id : 2789, nombre : 'Alejandro', apellido: 'Vang', estado : 'Activo'},
+    {id : 3821, nombre : 'Alasdair', apellido: 'Wiley', estado : 'Inactivo'},
+    {id : 5521, nombre : 'Isabel', apellido: 'Jacobson', estado : 'Activo'},
+    {id : 3921, nombre : 'Amelie', apellido: 'Logan', estado : 'Activo'},
+    {id : 9421, nombre : 'Raees', apellido: 'Cox', estado : 'Activo'},
+    {id : 7421, nombre : 'Alexandra', apellido: 'Johnson', estado : 'Inactivo'},
+    {id : 6421, nombre : 'Zachery', apellido: 'England',estado : 'Activo'}
+  ]
 }
