74fa40a344d23e8cb95b084a66bd16675f4b15b9
Edgardo Ezequiel Silveira <103297004+ezesilveira@users.noreply.github.com>
2023-09-27 23:37:11 -0300

    Lista + Silveira

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'projecto';
 }
+
+enum UserStatus {
+  Activo = 'enabled',
+  Inactivo = 'disabled',
+}
+
+class Usuario {
+  id : number;
+  nombre : string;
+  estado : UserStatus;
+  inicioSesion : boolean = false;
+
+  constructor (id : number, nombre : string, estado? : UserStatus){
+    this.id = id;
+    this.nombre = nombre;
+    this.estado = estado || UserStatus.Activo;
+  }
+  iniciarSesion() {
+    this.inicioSesion = true;
+  }
+}
+
+const UsuarioJuan = new Usuario (1, 'Juan');
+const UsuarioMartin = new Usuario (2, 'Martin');
+
+UsuarioJuan.iniciarSesion();
+
+console.log(UsuarioJuan)
