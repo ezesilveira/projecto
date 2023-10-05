0a7e9378ad20fa8f645bce1280ef3e5cb00eaa2c
Edgardo Ezequiel Silveira <103297004+ezesilveira@users.noreply.github.com>
2023-10-05 03:38:15 -0300

    Forms + Silveira

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -11,8 +11,34 @@ userForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
   this.userForm = this.formBuilder.group({
-    emailform: this.formBuilder.control(''),
-    passwordform: this.formBuilder.control(''),
+    emailform: ['', [Validators.required, Validators.email]],
+    passwordform: ['', [Validators.required, Validators.minLength(6)]],
   });
+}
+
+get emailControl() {
+  return this.userForm.controls['emailform'];
+}
+get emailControlIsInvalid() {
+  return this.userForm.controls['emailform'].invalid && this.userForm.controls['emailform'].touched
+  ;
+}
+get emailControlIsValid() {
+  return this.userForm.controls['emailform'].valid && this.userForm.controls['emailform'].touched
+  ;
+}
+
+get passwordControl () {
+  return this.userForm.controls['passwordform'];
+}
+get passwordControlIsInvalid() {
+  return this.userForm.controls['passwordform'].invalid && this.userForm.controls['passwordform'].touched;
+}
+get passwordControlIsValid() {
+  return this.userForm.controls['passwordform'].valid && this.userForm.controls['passwordform'].touched;
+}
+
+Onsubmit() : void {
+  console.log(this.userForm);
 }
 }
